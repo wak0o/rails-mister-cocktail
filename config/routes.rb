f035135a7e0aebc0ac80c1e25d035cc5b4832144
Antoine Wachowiak <antoinewachowiak@gmail.com>
2017-05-19 13:11:37 +0200

    All without complete views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:show, :index, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: [:destroy]
+
+  root "cocktails#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518112352) do
+ActiveRecord::Schema.define(version: 20170519102200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 20170518112352) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "cocktail_id"
     t.bigint "ingredient_id"
+    t.bigint "cocktail_id"
     t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
     t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
